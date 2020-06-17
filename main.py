b12669004661a49c66c470e0a451283569190e6e
ebizo <makoto71012@yahoo.co.jp>
2020-06-17 19:49:41 +0900

    ラインの言葉追加if文条件分岐messageでif

--- main.py
+++ main.py
@@ -44,10 +44,13 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        # TextSendMessage(text=event.message.text))
-        TextSendMessage(text="mako"))
+    message = "hello"
+    if re.match(r"こんにちは",event.message.text):
+        message = "hi"
+        line_bot_api.reply_message(
+            event.reply_token,
+            # TextSendMessage(text=event.message.text))
+            TextSendMessage(text="mako"))
 
 if __name__ == "__main__":
 #    app.run()
